a57b3dcf3555738906282a22448e6dcd8e3072ae
camilarevvth <camila.fc1@puccampinas.edu.br>
2026-07-01 19:14:00 -0300

    Move login panel higher

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "minha-estante-br-v8";
+const CACHE_NAME = "minha-estante-br-v10";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=5",
+  "./styles.css?v=7",
   "./app.js?v=6",
   "./firebase-config.js?v=2",
   "./manifest.webmanifest",
